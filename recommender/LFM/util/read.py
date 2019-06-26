9d475d77b30d0c95bcbd6b86b3df1b3efa7a0c32
TobiahShaw <a5414994@outlook.com>
2019-06-26 08:07:55 +0800

    func of get train data

--- recommender/LFM/util/read.py
+++ recommender/LFM/util/read.py
@@ -42,9 +42,9 @@ def get_ave_score(input_file):
     """
     get item ave score
     Args:
-        input_file: item info file
+        input_file: user item rating file
     Return:
-        a dict key : itemId, ave score
+        a dict key : itemId, value : ave score
     """
     if not os.path.exists(input_file):
         return {}
@@ -73,6 +73,49 @@ def get_ave_score(input_file):
 
     return score_dict
 
+def get_train_data(input_file):
+    """
+    get train data for lfm model train
+    Args:
+        input_file: user item rating file
+    Return:
+        a list [(userid, itemid, label)]
+    """
+    if not os.path.exists(input_file):
+        return []
+    score_dict = get_ave_score(input_file)
+    neg_dict = {}
+    pos_dict = {}
+    train_data = []
+    score_thr = 4.0
+    linenum = 0
+    fp = open(input_file,'r', encoding='UTF-8')
+    for line in fp:
+        if linenum == 0:
+            linenum += 1
+            continue
+        item = line.strip().split(',')
+        if len(item) != 4:
+            continue
+        userId,itemId,rating = item[0], item[1], float(item[2])
+        if userId not in pos_dict:
+            pos_dict[userId] = []
+        if userId not in neg_dict:
+            neg_dict[userId] = []
+        if rating >= score_thr:
+            pos_dict[userId].append((itemId, 1))
+        else:
+            score = score_dict.get(itemId, 0)
+            neg_dict[userId].append((itemId, score))
+    fp.close()
+    for userId in pos_dict:
+        data_num = min(len(pos_dict[userId]), len(neg_dict.get(userId,[])))
+        if data_num == 0:
+            continue
+        train_data += [(userId, tmp[0], tmp[1]) for tmp in pos_dict[userId][:data_num]]
+        sorted_neg_list = sorted(neg_dict[userId], key=lambda element:element[1], reverse=True)[:data_num]
+        train_data += [(userId, tmp[0], 0) for tmp in sorted_neg_list]
+    return train_data
 
 if __name__ == "__main__":
     item_dict= get_item_info("D:\\WorkSpace\\python\\tensorflow_hello\\recommender\\data\\ml-latest-small\\movies.csv")
@@ -83,3 +126,7 @@ if __name__ == "__main__":
     score_dict = get_ave_score("D:\\WorkSpace\\python\\tensorflow_hello\\recommender\\data\\ml-latest-small\\ratings.csv")
     print(len(score_dict))
     print(score_dict['31'])
+
+    train_data = get_train_data("D:\\WorkSpace\\python\\tensorflow_hello\\recommender\\data\\ml-latest-small\\ratings.csv")
+    print(len(train_data))
+    print(train_data[:20])
