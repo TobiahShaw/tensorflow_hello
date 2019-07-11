4d751393f9334f72c82599c26ea58f21083525f7
TobiahShaw <a5414994@outlook.com>
2019-07-11 08:17:50 +0800

    cal item sim

--- recommender/item2vec/produce_item_sim.py
+++ recommender/item2vec/produce_item_sim.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import operator
 
 def load_item_vec(input_file):
     if not os.path.exists(input_file):
@@ -20,3 +21,26 @@ def load_item_vec(input_file):
         item_vec[item] = np.array([float(ele) for ele in item[1:]])
     fp.close()
     return item_vec
+
+def cal_item_sim(item_vec, item_id, output_file, topK = 10):
+    if item_id not in item_vec:
+        return
+    score = {}
+    fix_item_vec = item_vec[item_id]
+    for tmp_item_id in item_vec:
+        if tmp_item_id == item_id:
+            continue
+        tmp_item_vec = item_vec[tmp_item_id]
+        divider = np.linalg.norm(fix_item_vec) * np.linalg.norm(tmp_item_vec)
+        if divider == 0:
+            score[tmp_item_id] = 0
+        else:
+            score[tmp_item_id] = round(np.dot(fix_item_vec, tmp_item_vec) / divider, 3)
+    fw = open(output_file, "w+", encoding='UTF-8')
+    out_str = item_id + '\t'
+    tmp_list = []
+    for comb in sorted(score.items(), key=operator.itemgetter(1), reverse=True)[:topK]:
+        tmp_list.append(comb[0] + '_' + comb[1])
+    out_str += ";".join(tmp_list)
+    fw.write(out_str + '\n')
+    fw.close()
