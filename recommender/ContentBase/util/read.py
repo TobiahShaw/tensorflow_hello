94dd1f13d761d13d57f544ccae4677e24dc56545
TobiahShaw <a5414994@outlook.com>
2019-07-12 08:18:25 +0800

    add func to get item'cate and cate-item arrays

--- recommender/ContentBase/util/read.py
+++ recommender/ContentBase/util/read.py
@@ -1,4 +1,5 @@
 import os
+import operator
 
 def get_ave_score(input_file):
     """
@@ -34,3 +35,54 @@ def get_ave_score(input_file):
         score_dict[itemId] = round(record_dict[itemId][1] / record_dict[itemId][0],3)
 
     return score_dict
+
+def get_item_category(ave_score, input_file):
+    """
+    get item category
+    Args:
+        ave_score: item average score
+        input_file: user item info file
+    Return:
+        a dict key : itemId, value : a dict key : category,value: ratio
+        a dict key : category, value: [item1, item2....]
+    """
+    if not os.path.exists(input_file):
+        return {}, {}
+    
+    item_cate={}
+    record = {}
+    cate_item_scort = {}
+
+    topK = 100
+
+    fp = open(input_file,'r', encoding='UTF-8')
+    linenum = 0
+    for line in fp:
+        if linenum == 0:
+            linenum += 1
+            continue
+        item = line.strip().split(',')
+        if len(item) < 3:
+            continue
+        itemid = item[0]
+        cate_str = item[-1]
+        cate_list = cate_str.strip().split('|')
+        ratio = round(1/len(cate_list), 3)
+        if itemid not in item_cate:
+            item_cate[itemid] = {}
+        for fix_cate in cate_list:
+            item_cate[itemid][fix_cate] = ratio
+    fp.close()
+    for tmp_item_id in cate_list:
+        for cate in cate_list[tmp_item_id]:
+            if cate not in record:
+                record[cate] = {}
+            item_ave_score = ave_score.get(tmp_item_id, 0)
+            record[cate][tmp_item_id] = item_ave_score
+    for cate in record:
+        if cate not in cate_item_scort:
+            cate_item_scort[cate] = []
+        for comb in sorted(record[cate].items(), key=operator.itemgetter(1), reverse=True)[:topK]:
+            cate_item_scort[cate].append(comb[0])
+    return item_cate, cate_item_scort
+
